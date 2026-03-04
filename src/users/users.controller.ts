795a81af28419fa59a4afd362e0b5dfe7a8cd68d
Yanzz231 <mrpartys05@gmail.com>
2026-03-04 08:31:45 +0700

    feat: dto for get profile

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Param } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UsersService } from './users.service';
+import { ProfileDto } from '../dto/users/profile.dto';
 
 @ApiTags('Users')
 @Controller('users')
@@ -9,6 +10,8 @@ export class UsersController {
 
   @Get(':id/profile')
   @ApiOperation({ summary: 'Get user profile by ID' })
+  @ApiResponse({ status: 200, type: ProfileDto })
+  @ApiResponse({ status: 404, description: 'User not found' })
   getProfile(@Param('id') id: string) {
     return this.usersService.getProfile(id);
   }
