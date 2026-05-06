38c9f7666d33d5301fcf6e84f1789b57c02e74bc
lixcoded <felixsky03@gmail.com>
2026-05-06 21:09:35 +0700

    feat : added module and main

---
+++ game-service/src/main.ts
@@ -0,0 +1,27 @@
+import 'dotenv/config';
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('WSpeedrun Game Service')
+    .setDescription('Manages games and run categories.')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(
+    Number(process.env.GAME_SERVICE_PORT ?? process.env.PORT ?? 3001),
+  );
+}
+bootstrap();
